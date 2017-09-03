6ec2af1e3369bf17d1aac2a784652c4c0bb5bdaf
giusecesa4 <31472608+giusecesa4@users.noreply.github.com>
2017-09-03 11:07:52 +0200

    changes 3/09

--- project_files/Files/asm_example/test.asm
+++ project_files/Files/asm_example/test.asm
@@ -1,6 +1,24 @@
-addi r1, r0, 100
-end:
+addi r1, r0,#100
+nop
+nop
+nop
+ciclo:
+subi r1,r1,#20
+addi r3,r3,#4
+nop
+nop
+beqz r1,ciclo
+nop
+nop
+nop
+addi r3,r3,#4
 j end
+nop
+nop
+nop
+addi r2,r2,#40
+end:
+add r1,r1,r3
 nop
 nop
 nop
