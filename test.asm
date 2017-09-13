4896bc802c47962d1662f5b63dd484eed7e7a691
giusecesa4 <31472608+giusecesa4@users.noreply.github.com>
2017-09-13 13:03:27 +0200

    try with other codes

--- test.asm
+++ test.asm
@@ -1,9 +1,22 @@
-addi r6,r0,4
-addi r1,r7,24
-subi r2,r1,18
-and r3,r2,r1
-xor r3,r2,r1
-ciclo:
-mult r6,r3,r6
-sll r5,r4,r6
-j ciclo
+addi r1,r0,100
+myloop:
+add r2,r2,r1
+addi r3,r0,10
+xori r3,r1,100
+subi r1,r1,20
+bnez r1,myloop
+nop
+addi r4,r3,2
+addi r21,r0,500
+addi r20,r0,4
+sw 4(r20), r20
+mult r5,r4,r3
+sgti r10,r4,4
+sll r10,r10,r1
+j end
+add r3,r3,r3
+sra r6,r4,r2
+end:
+lb r26,4(r20)
+lh r27,4(r20)
+lhu r28,4(r20)
